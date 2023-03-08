879e48addd2d9d8cd810b166c6fe7c89cfa45a76
Casey Chance <caseyjchance@gmail.com>
2023-03-07 17:37:12 -0800

    updates version

--- Formula/favicon.rb
+++ Formula/favicon.rb
@@ -5,7 +5,7 @@ class Favicon < Formula
   desc "A command to create favicons for a provided image file"
   homepage ""
   url "https://github.com/cachance7/utility-scripts.git", branch: "main"
-  version "0.1"
+  version "0.1.1"
   sha256 ""
   license ""
 
